b667e0928567c073e2f43b8fc7ffbd089614af19
liliang <1220795758@qq.com>
2024-07-29 20:04:04 +0800

    test: update test

--- test/app.js
+++ test/app.js
@@ -1,16 +1,11 @@
-const Cuka = require("../dist/cuka.js");
+const Cuka = require("cuka");
 
 const app = new Cuka();
 
-app.use(async (ctx) => {
-  console.log("Middleware executed");
-});
-
-app.on("/", "get", async (ctx) => {
-  ctx.setHeader("Content-Type", "text/plain");
-  ctx.end("Hello, World!");
-});
-
 app.listen(3000, () => {
   console.log("Server is listening on http://127.0.0.1:3000");
+});
+
+app.on("/", "get", (ctx) => {
+  ctx.end("Hello, World!");
 });
--- test/app.mjs
+++ test/app.mjs
@@ -2,15 +2,10 @@ import Cuka from "../dist/cuka.mjs";
 
 const app = new Cuka();
 
-app.use(async (ctx) => {
-  console.log("Middleware executed");
-});
-
-app.on("/", "get", async (ctx) => {
-  ctx.setHeader("Content-Type", "text/plain");
-  ctx.end("Hello, World!");
-});
-
 app.listen(3000, () => {
   console.log("Server is listening on http://127.0.0.1:3000");
+});
+
+app.on("/", "get", (ctx) => {
+  ctx.end("Hello, World!");
 });
